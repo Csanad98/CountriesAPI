7137901cffc58dee29590c00814d179e6d7cb9b2
Csanad98 <bakoscsanad@gmail.com>
2020-12-16 00:34:21 +0100

    change header building

--- Program.cs
+++ Program.cs
@@ -10,10 +10,12 @@ namespace CountriesAPI
     class Program
     {
         private static readonly HttpClient client = new HttpClient();
+        private const string baseURI = "https://restcountries-v1.p.rapidapi.com/";
+
         static async Task Main(string[] args)
         {
-            var countries = await GetCountriesByCallingCode(31);
-            Console.WriteLine(countries[0].Population);
+            var countries = await GetCountriesByCallingCode(1);
+            Console.WriteLine(countries[1].Name);
         }
 
         private static async Task<List<Country>> GetCountriesByCallingCode(int callingCode)
@@ -21,15 +23,18 @@ namespace CountriesAPI
             var request = new HttpRequestMessage
             {
                 Method = HttpMethod.Get,
-                RequestUri = new Uri("https://restcountries-v1.p.rapidapi.com/callingcode/"+callingCode.ToString()),
-                Headers =
-                {
-                    { "x-rapidapi-key", "87df6da5f0mshdc4ff4d39ed1d44p1696f0jsn20e0c00600ed" },
-                    { "x-rapidapi-host", "restcountries-v1.p.rapidapi.com" },
-                },
+                RequestUri = new Uri(baseURI+"/callingcode/"+callingCode.ToString())
             };
+            request.Headers.Add("x-rapidapi-key", "87df6da5f0mshdc4ff4d39ed1d44p1696f0jsn20e0c00600ed");
+            request.Headers.Add("x-rapidapi-host", "restcountries-v1.p.rapidapi.com");
             var response = await client.SendAsync(request);
-            response.EnsureSuccessStatusCode();
+            try {
+                response.EnsureSuccessStatusCode();
+            } catch (HttpRequestException e){
+                Console.Write(e.ToString());
+                System.Environment.Exit(1);
+            }
+            
             var body = await response.Content.ReadAsStreamAsync();
             var countries = await JsonSerializer.DeserializeAsync<List<Country>>(body);
 
